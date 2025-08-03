013887840c9a4c64de5cac747126856287de1349
Joyuv <givanilsonmariz7@outlook.com>
2025-08-02 22:24:46 -0300

    pytest que realmente testa

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,7 @@ def client():
 def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'<!DOCTYPE html>' in response.data  # ajuste conforme o retorno real
+    assert b'In\xc3\xadcio' in response.data  # ajuste conforme o retorno real
 
 def test_404_route(client):
     response = client.get('/rota-inexistente')
