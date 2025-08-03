51e8844fd7fb54b6dc6144ef0328c073d211b8f5
Joyuv <givanilsonmariz7@outlook.com>
2025-08-02 22:31:39 -0300

    correção de formatação black

--- app.py
+++ app.py
@@ -5,33 +5,40 @@ from models import User
 
 app = Flask(__name__)
 
-login_manager = LoginManager() 
-app.secret_key = 'guilherme'
+login_manager = LoginManager()
+app.secret_key = "guilherme"
 login_manager.init_app(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id, usuarios)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/dashboard', methods=['GET'])
+
+@app.route("/dashboard", methods=["GET"])
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template("dashboard.html")
 
-@app.route('/login')
+
+@app.route("/login")
 def login():
     return "Nada ainda"
 
-@app.route('/cadastro')
+
+@app.route("/cadastro")
 def cadastro():
     return "Nada ainda"
 
-@app.route('/produtos')
+
+@app.route("/produtos")
 def produtos_page():
     return "Nada ainda"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,20 +3,23 @@ import sys
 import os
 
 # Adiciona a raiz do projeto ao sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app  # ou from main import app
+
 
 @pytest.fixture
 def client():
     app.testing = True
     return app.test_client()
 
+
 def test_home_route(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert b'In\xc3\xadcio' in response.data  # ajuste conforme o retorno real
+    assert b"In\xc3\xadcio" in response.data  # ajuste conforme o retorno real
+
 
 def test_404_route(client):
-    response = client.get('/rota-inexistente')
+    response = client.get("/rota-inexistente")
     assert response.status_code == 404
