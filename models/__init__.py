037acbdbd475bfd766ef37a5f3ec860fd78b5f1f
Joyuv <givanilsonmariz7@outlook.com>
2025-08-03 09:33:25 -0300

    formtação dos arquivos com black

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from iniciar import get_db_conexao
 
+
 class User(UserMixin):
     def __init__(self, id, nome_usuario, senha):
         self.id = id
@@ -10,8 +11,11 @@ class User(UserMixin):
     @classmethod
     def get(cls, user_id):
         conn = get_db_conexao()
-        user_data = conn.execute('SELECT nome_usuario, senha FROM usuarios WHERE nome_usuario = ?', (user_id,)).fetchone()
+        user_data = conn.execute(
+            "SELECT nome_usuario, senha FROM usuarios WHERE nome_usuario = ?",
+            (user_id,),
+        ).fetchone()
         conn.close()
         if user_data:
-            return cls(user_data['nome_usuario'], user_data['senha'])
+            return cls(user_data["nome_usuario"], user_data["senha"])
         return None
